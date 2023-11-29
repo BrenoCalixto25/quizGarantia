921af4d6fd9a1ff91e774ae59561ffa2ee8c01e5
Breno Calixto de Santana <111536697+BrenoCalixto25@users.noreply.github.com>
2023-11-29 13:34:47 -0300

    Add files via upload

---
+++ JS/questions.js
@@ -0,0 +1,70 @@
+export default [
+  {
+    question: "1.	A partir de qual numeração inicia e finaliza os tamanhos dos anéis??",
+    answers: [
+      { option: "02 a 32", correct: true },
+      { option: "12 a 32", correct: false },
+      { option: "14 a 26", correct: false },
+      { option: "06 a 12", correct: false },
+    ],
+  },
+  {
+    question: "2. Quantos são os defeitos de fabricação coberto pela garantia?",
+    answers: [
+      { option: "7", correct: false },
+      { option: "6", correct: true },
+      { option: "9", correct: false },
+      { option: "5", correct: false },
+    ],
+  },
+  {
+    question: "3.	Qual o nome da plataforma usada para verificar descrições das peças??",
+    answers: [
+      { option: "NetWEb", correct: false },
+      { option: "RommanelWeb", correct: false },
+      { option: "CDNetWeb", correct: true },
+      { option: "InforWeb", correct: false },
+    ],
+  },
+  {
+    question: "4.	Qual o significado da borboleta Rommanel? ",
+    answers: [
+      { option: "Sabedoria", correct: false },
+      { option: "Carinho", correct: false },
+      { option: "Transformação", correct: true },
+      { option: "recomeço", correct: false },
+    ],
+  },
+  {
+    question: "5.	Em qual ano a Rommanel foi fundada? ",
+    answers: [
+      { option: "1995", correct: false },
+      { option: "1993", correct: false },
+      { option: "1996", correct: true },
+      { option: "1994", correct: false },
+    ],
+  },
+  {
+    question: "6.	É feito a troca da peça que apresenta quebra no ponto de solda sem verificar o estado da peça?",
+    answers: [
+      { option: "Sim", correct: false },
+      { option: "Não", correct: true },
+    ],
+  },
+  {
+    question: "7.	A Rommanel preza muito pelo estado da peça, é verdade?",
+    answers: [
+      { option: "Sim", correct: true },
+      { option: "Não", correct: false },
+    ],
+  },
+  {
+    question: "8.	Quantas camadas tem o folheado banhado a ouro Rommanel?",
+    answers: [
+      { option: "3", correct: false },
+      { option: "5", correct: false },
+      { option: "6", correct: true },
+      { option: "4", correct: false },
+    ],
+  },
+];
